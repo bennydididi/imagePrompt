6127db898ac22ae3054ff2b4258a1c7f234b0839
benny <424095116@QQ.com>
2026-01-25 17:18:26 +0800

    111

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -46,4 +46,7 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
+  skipValidation:
+    !!process.env.SKIP_ENV_VALIDATION ||
+    process.env.npm_lifecycle_event === "lint",
 });
